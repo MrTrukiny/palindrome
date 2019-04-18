99d7ced793edcb62db29335dffb27b30c882b341
MrTrukiny <carloseduardo.tkn@gmail.com>
2019-04-17 19:02:35 -0500

    Finish working and refactored palindrome method

--- index3.js
+++ index3.js
@@ -18,11 +18,18 @@ function Phrase(content) {
   // For example:
   //   new Phrase("Hello, world!").letters() === "Helloworld"
   this.letters = function letters() {
-    return (this.content.match(/[a-z]/gi) || []).join("");
+    let theLetters = [];
+    const letterRegex = /[a-z]/i;
+    Array.from(this.content).forEach(function(character) {
+      if (character.match(letterRegex)) {
+        theLetters.push(character);
+      }
+    });
+    return theLetters.join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
   }
-}   
+}
